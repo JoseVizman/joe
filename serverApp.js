6e37cbfc8f0069772399497fd237145edf2990c2
JoseVizman <37042068+JoseVizman@users.noreply.github.com>
2018-12-19 19:19:05 +0100

    Add files via upload

---
+++ serverApp.js
@@ -0,0 +1,26 @@
+var http = require('http'),
+      fs = require('fs'),
+     url = require('url');
+
+http.createServer(function(request, response){
+    /* Z požadavku vytáhnout path, který jsem si v klientu označil 
+    (může se to jmenovat jakkoliv), abych zde poznal, odkud 
+    tento požadavek je a o co se žádá*/
+    var path = url.parse(request.url).pathname;
+    if(path=="/getstring"){
+        fs.readFile('./file.json', function(err, file) {
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end(file, "utf-8");
+        });
+    }else{
+        fs.readFile('./index.html', function(err, file) {  
+            if(err) {  
+                // write an error response or nothing here  
+                return;  
+            }  
+            response.writeHead(200, { 'Content-Type': 'text/html' });  
+            response.end(file, "utf-8");  
+        });
+    }
+}).listen(8080);
+console.log("server initialized");
